6dbfeddab8ea29bef21dfb8ef24000930cd154b7
Sandipan Mahata <Sandipan.m@muvin.in>
2024-07-27 11:13:15 +0530

    set up s3 reverse proxy

---
+++ s3-reverse-proxy/index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const httpProxy = require("http-proxy")
+
+const app = express()
+const PORT = 8000
+
+const proxy = httpProxy.createProxy()
+
+app.use((req, res) => {
+    const hostName = req.hostname
+    const subdomain = hostName.split(".")[0]
+
+    const resolvesTo = `https://versel-clone-sss-bucket.s3.ap-south-1.amazonaws.com/outputs/${subdomain}`
+
+    return proxy.web(req, res, {target: resolvesTo, changeOrigin: true})
+})
+
+proxy.on("proxyReq", (proxyReq, req, res) => {
+    const url = req.url
+
+    if(url === '/')
+        proxyReq.path += "index.html"
+})
+
+app.listen(PORT, () => console.log(`App is listening on port ${PORT}`))
