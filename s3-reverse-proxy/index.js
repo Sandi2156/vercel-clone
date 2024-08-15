72ad1f5031fe4bafba113cfa5e8b1db749150c5b
Sandipan Mahata <Sandipan.m@muvin.in>
2024-08-15 11:15:12 +0530

    refactored reverse proxy

--- s3-reverse-proxy/index.js
+++ s3-reverse-proxy/index.js
@@ -1,25 +1,44 @@
-const express = require("express")
-const httpProxy = require("http-proxy")
+const express = require("express");
+const httpProxy = require("http-proxy");
 
-const app = express()
-const PORT = 8000
+const app = express();
+const PORT = process.env.PORT || 8000;
 
-const proxy = httpProxy.createProxy()
+const proxy = httpProxy.createProxy();
 
 app.use((req, res) => {
-    const hostName = req.hostname
-    const subdomain = hostName.split(".")[0]
+  const hostName = req.hostname;
+  const subdomain = hostName.split(".")[0];
 
-    const resolvesTo = `https://versel-clone-sss-bucket.s3.ap-south-1.amazonaws.com/outputs/${subdomain}`
+  const resolvesTo = `https://versel-clone-sss-bucket.s3.ap-south-1.amazonaws.com/outputs/${subdomain}`;
 
-    return proxy.web(req, res, {target: resolvesTo, changeOrigin: true})
-})
+  console.log(`Proxying request for subdomain: ${subdomain}`);
+  console.log(`Target URL: ${resolvesTo}`);
+
+  return proxy.web(
+    req,
+    res,
+    { target: resolvesTo, changeOrigin: true },
+    (err) => {
+      if (err) {
+        console.error("Proxy error:", err);
+        res.status(500).send("Proxy error");
+      }
+    }
+  );
+});
 
 proxy.on("proxyReq", (proxyReq, req, res) => {
-    const url = req.url
+  const url = req.url;
 
-    if(url === '/')
-        proxyReq.path += "index.html"
-})
+  if (url === "/") proxyReq.path += "index.html";
 
-app.listen(PORT, () => console.log(`Proxy is listening on port ${PORT}`))
+  console.log(`Modified request path: ${proxyReq.path}`);
+});
+
+proxy.on("error", (err, req, res) => {
+  console.error("Proxy error event:", err);
+  res.status(500).send("Proxy encountered an error");
+});
+
+app.listen(PORT, () => console.log(`Proxy is listening on port ${PORT}`));
